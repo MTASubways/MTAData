ca87cf0d31325a712f6df1c102305c042395c384
Phillippa Thomson <phillippa.thomson@gmail.com>
2016-08-04 14:59:43 -0400

    updated api

--- mta_app/MTA_app1.py
+++ mta_app/MTA_app1.py
@@ -2,33 +2,36 @@
 # It contains the definition of routes and views for the application.
 
 import flask
-# import numpy as np
-# import pandas as pd
-# from sklearn.ensemble import RandomForestClassifier
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 
 #---------- MODEL IN MEMORY ----------------#
 
-# # Read in the titanic data and build a model on it
+# Read in the titanic data and build a model on it
 # df = pd.read_csv('data/titanic.csv')
-# include = ['Pclass', 'Sex', 'Age', 'Fare', 'SibSp', 'Survived']
-#
-# # Create dummies and drop NaN
-# df['Sex'] = df['Sex'].apply(lambda x: 0 if x == 'male' else 1)
-# df = df[include].dropna()
-#
-# X = df[['Pclass', 'Sex', 'Age', 'Fare', 'SibSp']]
-# y = df['Survived']
-#
-# PREDICTOR = RandomForestClassifier(n_estimators=100).fit(X, y)
+# include = ['day', 'month', 'hour', 'ent', 'exit']
 
+# X = df[['day', 'month', 'hour']]
+# y1 = df['ent']
+# y2 = df['exit']
 
-
+# PREDICTOR1 = LinearRegression().fit(X, y1)
+# PREDICTOR2 = LinearRegression().fit(X, y2)
 
 
 #---------- CREATING AN API, METHOD 1 ----------------#
-
+ 
 # Initialize the app
 app = flask.Flask(__name__)
+
+with open('entries.pkl', 'r') as picklefile:
+    PREDICTOR1 = pickle.load(picklefile)
+
+with open('exits.pkl', 'r') as picklefile:
+    PREDICTOR2 = pickle.load(picklefile)
 
 
 # When you navigate to the page 'server/predict', this will run
@@ -39,16 +42,34 @@ app = flask.Flask(__name__)
 @app.route('/predict', methods=["GET"])
 def predict():
     '''Makes a prediction'''
+
+    result1 = []
+    result2 = []
     day = float(flask.request.args['day'])
     month = float(flask.request.args['month'])
-    station = float(flask.request.args['station'])
+    for i in range(4,25,4):
+        hour = i
+        item = np.array([day,month,hour])
+        # item = np.array([1,2,hour])
+        score1 = PREDICTOR1.predict(item)[0]
+        score2 = PREDICTOR2.predict(item)[0]
+        result1.append(score1)
+        result2.append(score2)
+    
+    # station = float(flask.request.args['station'])
 
-    # item = np.array([pclass, sex, age, fare, sibsp])
+
+    # item = np.array([day,month,hour])
+    # score1 = PREDICTOR1.predict_proba(item)
+    # score2 = PREDICTOR2.predict_proba(item)
     # score = PREDICTOR.predict_proba(item)
     # results = {'survival chances': score[0,1], 'death chances': score[0,0]}
-    # results = {'0i':1000,'0o':2000,'4i':1000,'4o':2500,'8i':1000,'8o':2000,'12i':1000,'12o':2000,'16i':1000,'16o':2300,'20i':1000,'20o':2000}
 
-    results = {"in":{"0":1000,"4":1500,"8":1000,"12":1000,"16":1000,"20":1000,"24":1000},"out":{"0":1000,"4":1500,"8":1000,"12":1000,"16":1000,"20":1000,"24":1000}}
+    # results = {"in":{"0":1000,"4":1500,"8":1000,"12":1000,"16":1000,"20":1000,"24":1000},"out":{"0":1000,"4":1500,"8":1000,"12":1000,"16":1000,"20":1000,"24":1000}}
+    # results = {"in":{,1500,1000,1000,1000,1000,1000},"out":{2000,2500,3000,2000,2000,1200,1200}}
+    results = {"in":result1,"out":result2}
+    
+
     return flask.jsonify(results)
 
 
@@ -84,7 +105,7 @@ def predict():
 if __name__ == '__main__':
     '''Connects to the server'''
 
-    HOST = '0.0.0.0'
+    HOST = '127.0.0.1'
     PORT = '4000'
 
     app.run(HOST, PORT)
